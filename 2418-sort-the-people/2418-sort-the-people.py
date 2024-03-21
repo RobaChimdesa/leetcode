2013992b6baa7c1113ccffd2e37ab15459d260ed
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2024-03-21 20:39:09 +0300

    Time: 749 ms (5.07%), Space: 16.9 MB (99.68%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,30 +1,11 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # allPeople = {}
-        # originalNames = {}
-        # for i in range(len(names)):
-        #     if names[i] in allPeople:
-        #         # add '1' to the end of the name to signify that its still a new person -> ppl can have same names 
-        #         allPeople[names[i] + str(i)] = heights[i]
-        #         originalNames[names[i] + str(i)] = names[i]
-        #     else:
-        #         allPeople[names[i]] = heights[i]
-        #         originalNames[names[i]] = names[i]
-
-        # sortedVals = dict(sorted(allPeople.items(), key = lambda item: item[-1], reverse=True))
-        # #return sortedVals
-        # final = []
-        # for names in sortedVals:
-        #     final.append(originalNames[names])
-        # return final
-        people={}
-        ordered=[]
-        for i in range (len(names)):
-
-            people[heights[i]]=names[i]
-        sortedpeople=dict(sorted(people.items(),key=lambda item:item[0], reverse=True))
-        for name in sortedpeople:
-
-            ordered.append(people[name])
-        return ordered
-        
+        n = len(heights)
+        for i in range (n):
+            for j in range(n-i-1):
+                if heights[j] < heights[j+1]:
+                    heights[j],heights[j+1] = heights[j+1],heights[j]
+                    names[j],names[j+1] = names[j+1],names[j]
+        return names            
+     
+       
