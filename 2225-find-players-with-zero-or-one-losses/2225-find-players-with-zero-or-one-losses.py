e3d2af932af855d66f762b47e0d16bddb7dae081
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2024-04-27 11:20:56 +0300

    Time: 1377 ms (79.23%), Space: 71.2 MB (59.71%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,12 +1,16 @@
 class Solution:
     def findWinners(self, matches):
-        lost = defaultdict(int)
-        
-        for m in matches:
-            lost[m[0]] += 0
-            lost[m[1]] += 1
-        
-        zero = sorted([k for k, l in lost.items() if l == 0])
-        ones = sorted([k for k, l in lost.items() if l == 1])
-        
-        return [zero, ones]
+        lose = defaultdict(int)
+        print(lose)
+        zero,one = [],[]
+        for game in matches:
+            lose[game[0]] += 0
+            lose[game[1]] += 1
+        zero = sorted([k for k,l in lose.items() if l == 0])
+        one =  sorted([k for k,l in lose.items() if l ==1])
+        return [zero,one]    
+           
+                 
+                          
+            
+
