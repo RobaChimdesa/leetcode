5d86bb459b00aea6ca6337715b63a46582fac2d1
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2024-04-28 22:37:46 +0300

    Time: 122 ms (79.43%), Space: 18.1 MB (31.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,7 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
+
         """
         j=0
         for i in range(len(nums)):
@@ -10,8 +9,22 @@ class Solution:
 
             if nums[j] !=0:
                 j+=1
+        Do not return anything, modify nums in-place instead.
+        """
+        read = 0 
+        write = 0
+        while read < len(nums):
 
+            if nums[read] != 0:
+                temp = nums[read]
+                nums[read] = nums[write]
+                nums[write] = temp
+                write+=1
+            read+=1
+        return nums        
 
+                
+       
 
 
             
